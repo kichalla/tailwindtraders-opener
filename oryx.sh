09ba7702ab79ebb6548555fb8b3762c9817df2c0
Kiran Challa <kichalla@microsoft.com>
2019-03-12 04:45:17 -0700

    Enabled installing new oryx binary

--- oryx.sh
+++ oryx.sh
@@ -1,30 +1,30 @@
 #!/bin/bash
 
-# # ORYX_ALT_COMMIT_ID='???'
-# BUILD_DEST='/tmp/buildscriptgen'
+# ORYX_ALT_COMMIT_ID='???'
+BUILD_DEST='/tmp/buildscriptgen'
 
-# # Cleanup 
-# rm -rf $BUILD_DEST && mkdir -p $BUILD_DEST
-# rm -rf /tmp/Oryx
+# Cleanup 
+rm -rf $BUILD_DEST && mkdir -p $BUILD_DEST
+rm -rf /tmp/Oryx
 
-# # Fetch source code 
-# cd /tmp && git clone https://github.com/kichalla/Oryx.git
-# cd Oryx
-# #git checkout –q $ORYX_ALT_COMMIT_ID
+# Fetch source code 
+cd /tmp && git clone https://github.com/kichalla/Oryx.git
+cd Oryx
+#git checkout –q $ORYX_ALT_COMMIT_ID
 
-# # Temporary workaround to `/var/nuget` not being writable
-# export NUGET_PACKAGES=/tmp/nuget
-# mkdir -p $NUGET_PACKAGES
+# Temporary workaround to `/var/nuget` not being writable
+export NUGET_PACKAGES=/tmp/nuget
+mkdir -p $NUGET_PACKAGES
 
-# # Build 
-# cd src
-# GIT_COMMIT=$ORYX_ALT_COMMIT_ID \
-#     dotnet publish -r linux-x64 -o $BUILD_DEST -c Release BuildScriptGeneratorCli/BuildScriptGeneratorCli.csproj
-# echo
+# Build 
+cd src
+GIT_COMMIT=$ORYX_ALT_COMMIT_ID \
+    dotnet publish -r linux-x64 -o $BUILD_DEST -c Release BuildScriptGeneratorCli/BuildScriptGeneratorCli.csproj
+echo
 
-# # Run 
-# oryx="$BUILD_DEST/GenerateBuildScript"
-# $oryx --version
+# Run 
+oryx="$BUILD_DEST/GenerateBuildScript"
+$oryx --version
 
 #----------------------------------------------------------------
 
